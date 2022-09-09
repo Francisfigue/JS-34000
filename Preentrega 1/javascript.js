f2c7dee77bf86990cfc115d2046bdf44a33cd4b6
Francisfigue <107960704+Francisfigue@users.noreply.github.com>
2022-09-08 21:59:33 -0300

    primera preentrega
    subida de la primera pre entrega JS-34000

---
+++ Preentrega 1/javascript.js
@@ -0,0 +1,110 @@
+let productos = [ 
+    {Id: 1,
+    nombre: "harina 000",
+    precioKg: 250,
+    stock: 100,   
+},
+    {Id: 2,
+    nombre: "harina 0000",
+    precioKg: 250,
+    stock: 100,   
+},
+    {Id: 3,
+    nombre: "harina integral",
+    precioKg: 300,
+    stock: 75,   
+},
+    {Id: 4,
+    nombre: "poroto blanco",
+    precioKg: 700,
+    stock: 60,   
+},
+    {Id: 5,
+    nombre: "poroto colorado",
+    precioKg: 620,
+    stock: 45,   
+},
+    {Id: 6,
+    nombre: "poroto negro",
+    precioKg: 500,
+    stock: 100,   
+},
+    {Id: 7,
+    nombre: "copos maiz",
+    precioKg: 350,
+    stock: 50,   
+},
+    {Id: 8,
+    nombre: "cereal chocolate",
+    precioKg: 450,
+    stock: 65,   
+},
+    {Id: 9,
+    nombre:"granola mix",
+    precioKg: 850,
+    stock: 50,   
+},
+    {Id: 10,
+    nombre: "granola crocante",
+    precioKg:1050,
+    stock: 40,   
+},
+]
+
+let carrito = [
+    {Id: 9,
+        nombre:"granola mix",
+        precioKg: 850,
+        stock: 50,   
+    },
+    {Id: 6,
+        nombre: "poroto negro",
+        precioKg: 500,
+        stock: 100,   
+    },
+    {Id: 2,
+        nombre: "harina 0000",
+        precioKg: 250,
+        stock: 100, }
+]
+
+console.table (productos)
+
+
+function filtrarProductos() {
+    let parametro = prompt("Ingresa el producto buscado:")
+    let resultado = productos.filter((producto)=> producto.nombre.includes(parametro))
+    console.table(resultado)}
+
+
+function ordenarProductosPrecio() {
+    productos.sort((a, b)=> {
+        if (a.precio > b.precio) {
+            return 1
+        }
+        if (a.precio < b.precio) {
+            return -1
+        }
+        return 0
+    })
+    console.table(productos)
+}
+
+function ordenarProductosStock() {
+    productos.sort ((a,b) => {
+        if (a.stock > b.stock) {
+            return 1
+        }
+        if (a.stock < b.stock) {
+            return -1
+        }
+        return 0
+    })
+    console.table(productos)
+}
+
+let envio = 550
+function calcularTotal() {
+    let total = carrito.reduce((acumulador, producto)=>  acumulador + producto.precioKg, envio)
+        console.log("Total del carrito + envio:", total)
+    }
